d21015dda4584012d94037ab8f68f1eed7bb28bb
ansikapandey943-ui <ansikapandey943@gmail.com>
2026-01-02 06:27:58 +0530

    Add files via upload

---
+++ employee_salary.java
@@ -0,0 +1,12 @@
+class employee_salary{
+    public static void main(String[] args){
+    int emp_id=210;
+    int working_days=29;
+    double salary_per_day=2380.56;
+    double total_salary= working_days*salary_per_day;
+    boolean emp_status=true;
+    System.out.println("The total salary:"+ total_salary);
+    System.out.println("Is the employee permanent?:" + emp_status);
+    }
+}
+   
---
+++ student_details.java
@@ -0,0 +1,16 @@
+class student_details{
+     public static void main(String[] args){
+         int roll_no=69;
+         int age=18;
+         double score=90.23;
+         char grade='A';
+         boolean is_pass=true;
+         System.out.println("Roll no.:"+ roll_no);
+         System.out.println("Age:"+age);
+         System.out.println("Score:"+score);
+         System.out.println("Grade:"+ grade);
+         System.out.println("Passed?:"+is_pass);
+     }
+}
+
+
